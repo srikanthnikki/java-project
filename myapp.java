453b823abc8c358388b1228851ff1d05af06575c
sri123 <pulavenisrikanth01@gmail.com>
2025-08-03 07:58:18 +0000

    updated-code

--- myapp.java
+++ myapp.java
@@ -2,6 +2,9 @@ public class Myapp {
 	public static void main(string[ args){
 		int a=10;
 		int b=20;
-		system.out.println("addition is:",+(a+b));
+		int c=30;
+		int d=40;
+		system.out.println("addition is:",+(a+b+c+d));
+		system.out.println("the task complryrd by srikanthyadav")
 	}
 }	
