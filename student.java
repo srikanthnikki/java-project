7f098fb6312aee5335cc96be7e5edfb728089138
srikanthnikki <pullavenisrikanth01@gmail.com>
2025-08-04 19:16:26 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,16 @@
+public class student {
+   public static void main(string[] args) {
+       int rollno = 1596;
+       string sname = "Srikanth";
+       string course = "Python";
+       system.out.println("learn here and lead anywhere");
+       system.out.println("my name is: " +sname);
+       system.out.println("my course is: " + course);
+       system.out.println("my roll no is: " + rollno);
+       system.out.println("=======================");
+       system.out.println("good mng");
+       system.out.println("WELCOME TO JENKINS");
+       system.out.println("good mng");
+       system.out.println("good mng");
+   }
+}
